c986f9492c978438c4c055dc0536f42927f44482
Victor Dubois <victor.dubois@free.fr>
2023-05-19 10:43:13 +0200

    odometry tuned :)

--- Inc/constants.h
+++ Inc/constants.h
@@ -10,8 +10,8 @@
 
 #define TICKS_PER_REVOLUTION 4096 //360    // Nb ticks per wheel revolution
 #define WHEEL_DIAM 68               // Diameter of the wheel (mm) UNUSED !!!
-#define DIST_PER_REVOLUTION 206.5//214.635// 210.481 //304.734 // Distance traveled for a full wheel revolution (in mm)
-#define TICKS_PER_DEG 91.108345 // 91.001144 // 91.055 	// 89.28//89//89.06 //4.86          // Nb of diff tick (enc1 - enc2) it takes to rotate 1 deg (old value: 4.7)
+#define DIST_PER_REVOLUTION 216.2695 // 217.879 // 214.66 // 206.5//214.635// 210.481 //304.734 // Distance traveled for a full 	wheel revolution (in mm)
+#define TICKS_PER_DEG 91.472 // 91.290 // 91.108345 // 91.001144 // 91.055 	// 89.28//89//89.06 //4.86          // Nb of diff tick (enc1 - enc2) it takes to rotate 1 deg (old value: 4.7)
 #define TICKS_OVERFLOW 65536
 #define TICKS_half_OVERFLOW TICKS_OVERFLOW/2
 #endif
